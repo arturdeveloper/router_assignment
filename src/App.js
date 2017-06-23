788485d69da99c96021b81f40cc7d5cb2e09b56c
arturdeveloper <sato.alta@gmail.com>
2017-06-23 08:21:40 -0400

    wrong usage of async/await

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 
 // offline data
-import data from './data.json';
+import data from "./data.json";
 
 const Header = styled.div`
 display: flex;
@@ -30,111 +30,113 @@ background-color: hsla(115, 40%, 50%, 1);
 color: white;
 `;
 
-const currency = ['usd', 'eur', 'gbp', 'jpy'];
+const currency = ["usd", "eur", "gbp", "jpy"];
 
 // const url = 'http://api.fixer.io/latest?base=USD';
-const url = 'http://api.fixer.io/latest?base=';
-
+const url = "http://api.fixer.io/latest?base=";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.handler=this.handler.bind(this);
+  constructor() {
+    super();
+    this.handler = this.handler.bind(this);
+  }
+  state = { data: {}, fullData: {} };
 
+  async load_data(path) {
+    try {
+    //   const req = await fetch(path);
+    //   const results = await req.json();
+
+      // offline data
+      // const results = data;
+      // console.log(results.rates);
+
+      // error /forEach/ or /map/ functions are synchoronous that means we can't use /async fetch/ inside it
+      let fullData = currency.forEach((base)=> {
+          let req = await fetch(path + base);
+          let results = await req.json();
+          
+          return(results)
+      });
+
+
+//-------------------------------
+    //   this.setState({ data: results });
+    //   this.setState({ data: fullData });
+    } catch (error) {
+      alert(error.message);
     }
-    state = {data: {}}
+  }
 
-    async load_data(path) {
+  componentDidMount() {
+    this.load_data(url + currency[0]);
+  }
 
-        try {
-                
-            const req = await fetch(path);
-            const results = await req.json();
+  handler(e) {
+    const arg = e.target.textContent;
+    this.load_data(url + arg);
+  }
 
-            // offline data
-            // const results = data;
-            // console.log(results.rates);
-
-            /*// TODO. why gives error - function not found
-            var rates = [];
-            for (var key in results.rates){
-                let wholeObj = {};
-                wholeObj[key] = results.rates[key];
-                let wholeArr = [key, results.rates[key]];
-                rates.push(wholeArr);
-            }
-             this.setState({data: rates});
-             */
-
-            this.setState({data: results});
-
-        } catch (error) {
-            alert(error.message);
-        }
-    }
-
-    componentDidMount() {
-        this.load_data(url + currency[0]);
-    }
-
-    handler(e) {
-        const arg = e.target.textContent;
-        this.load_data(url + arg);
-    }
-
-    render(){
-
-        let headers = currency.map((elem) => {
-            return (
-                <Link style={{color: 'white', textDecoration: 'none'}} key={elem} to={elem} onClick={this.handler}>
-                    <h2>{elem.toUpperCase()}</h2>
-                </Link>
-            )
-        });
-        let routes = currency.map((elem) => {
-            return (
-                <Route key={elem} exact path={`/${elem}`} component={()=> <CartItems base={elem} data={this.state.data} />} />
-            )
-        });
-        // console.log(this.state.data);
-        return(
-            <Router>
-            <div>
-            <Header>
-                {headers}
-            </Header>
-            <Cart>
-                <Route exact path={'/'} component={()=> <CartItems data={this.state.data} />} />
-                {routes}
-            </Cart>
-            </div>
-            </Router>
-        )
-    }
+  render() {
+    let headers = currency.map(elem => {
+      return (
+        <Link
+          style={{ color: "white", textDecoration: "none" }}
+          key={elem}
+          to={elem}
+          onClick={this.handler}
+        >
+          <h2>{elem.toUpperCase()}</h2>
+        </Link>
+      );
+    });
+    let routes = currency.map(elem => {
+      return (
+        <Route key={elem} exact path={`/${elem}`} render={() =>         <CartItems base={elem} data={this.state.data} />} />
+      );
+    });
+    // console.log(this.state.data);
+    return (
+      <Router>
+        <div>
+          <Header>
+            {headers}
+          </Header>
+          <Cart>
+            <Route exact path={"/"} render={() => 
+                <CartItems data={this.state.data} />} />
+            {routes}
+          </Cart>
+        </div>
+      </Router>
+    );
+  }
 }
 
-const CartItems =(props)=> {
+const CartItems = props => {
+  var rates = [];
+  for (var key in props.data.rates) {
+    let wholeArr = [key, props.data.rates[key]];
+    rates.push(wholeArr);
+  }
 
-    var rates = [];
-    for (var key in props.data.rates){
-        let wholeArr = [key, props.data.rates[key]];
-        rates.push(wholeArr);
-    }
+  console.log(rates);
 
-    console.log(rates);
-
-    const showItems = rates.map((item)=> {
-        return (
-            <li style={{color: 'hsla(60, 100%, 50%, 1)'}} key={item[0]}><h2>{item[0]} : {item[1]}</h2></li>
-        )
-    });
-
+  const showItems = rates.map(item => {
     return (
+      <li style={{ color: "hsla(60, 100%, 50%, 1)" }} key={item[0]}>
+        <h2>{item[0]} : {item[1]}</h2>
+      </li>
+    );
+  });
+
+  return (
     <div>
-    <ul>
-    {showItems}
-    </ul>
+      <ul>
+        {showItems}
+      </ul>
     </div>
-)};
+  );
+};
 
 export default App;
